fc033b06aa6564aee0d14716c3621d1679072b99
p8ul <pkinuthia10@gmail.com>
2018-02-08 18:08:52 +0300

    refine sitesettings

--- app_dir/static/js/site/index.js
+++ app_dir/static/js/site/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import InstitutionDetails from './components/InstitutionDetails';
+import SmsSettings from './components/SmsSettings';
 
+// basic settings
 ReactDOM.render(<InstitutionDetails />, document.getElementById('basic'));
+
+// sms settings
+ReactDOM.render(<SmsSettings />, document.getElementById('sms'));
