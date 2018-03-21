c42c81ad593d8276acb1a9fdcea98f154373a650
alexkiburu <alex.kiburu@glosoftgroup.com>
2018-03-21 16:51:47 +0300

    added passmark field to api, added validation for submission of exam setting details, added listing page for exam settings, added exam settings details page, added validation for the create api of eaxm settings (#41)

--- app_dir/modules/exams/configuration/api/serializers.py
+++ app_dir/modules/exams/configuration/api/serializers.py
@@ -1,7 +1,9 @@
 # site settings rest api serializers
 
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
 from ...configuration.models import ExamConfiguration as Table
+from ...configuration.models import Exam, Cat, Assignment
 from structlog import get_logger
 
 logger = get_logger(__name__)
@@ -15,6 +17,9 @@ class TableListSerializer(serializers.ModelSerializer):
     academicyear    = serializers.SerializerMethodField()
     academicclass    = serializers.SerializerMethodField()
     term    = serializers.SerializerMethodField()
+    assignments    = serializers.SerializerMethodField()
+    cats    = serializers.SerializerMethodField()
+    exams    = serializers.SerializerMethodField()
 
     class Meta:
         model = Table
@@ -23,7 +28,12 @@ class TableListSerializer(serializers.ModelSerializer):
                   'academicyear',
                   'academicclass',
                   'term',
-                  'percentage',
+                  'pass_marks',
+                  'total_marks',
+                  'is_percentage',
+                  'assignments',
+                  'cats',
+                  'exams',
                   'detail_url',
                   'update_url',
                   'delete_url'
@@ -40,6 +50,15 @@ class TableListSerializer(serializers.ModelSerializer):
     def get_term(self, obj):
         return obj.term.name
 
+    def get_cats(self, obj):
+        return obj.cat.count()
+
+    def get_assignments(self, obj):
+        return obj.assignment.count()
+
+    def get_exams(self, obj):
+        return obj.exam.count()
+
 
 class CreateListSerializer(serializers.ModelSerializer):
     exams = serializers.JSONField(write_only=True)
@@ -52,8 +71,17 @@ class CreateListSerializer(serializers.ModelSerializer):
                   'academicclass',
                   'term',
                   'is_percentage',
+                  'pass_marks',
+                  'total_marks',
                   'exams',
                  )
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Table.objects.all(),
+                fields=('subject', 'academicyear', 'academicclass', 'term'),
+                message=_("Exam Setting Already Exists.")
+            )
+        ]
 
     def create(self, validated_data):
         instance = Table()
@@ -67,11 +95,39 @@ class CreateListSerializer(serializers.ModelSerializer):
             instance.term = validated_data.get('term')
         if validated_data.get('is_percentage'):
             instance.is_percentage = validated_data.get('is_percentage')
+        if validated_data.get('pass_marks'):
+            instance.pass_marks = validated_data.get('pass_marks')
+        if validated_data.get('total_marks'):
+            instance.total_marks = validated_data.get('total_marks')
 
-        logger.info('details: ' + str(validated_data))
-        for i in validated_data.get('custom'):
-            logger.info(i)
-        # instance.save()
+        instance.save()
+
+        for i in validated_data.get('exams'):
+            if 'assignment' in str(i['name']):
+                logger.info(" assignment name-"+str(i['name'])+": value-"+str(i['value']))
+                assignment = Assignment()
+                assignment.examId = instance
+                assignment.name = i['name']
+                assignment.marks = i['value']
+                assignment.save()
+                continue
+            elif 'cat' in str(i['name']):
+                logger.info(" cat name-" + str(i['name']) + ": value-" + str(i['value']))
+                cat = Cat()
+                cat.examId = instance
+                cat.name = i['name']
+                cat.marks = i['value']
+                cat.save()
+                continue
+            elif 'exam' in str(i['name']):
+                logger.info(" exam name-" + str(i['name']) + ": value-" + str(i['value']))
+                exam = Exam()
+                exam.examId = instance
+                exam.name = i['name']
+                exam.marks = i['value']
+                exam.save()
+                continue
+
 
         return instance
 
