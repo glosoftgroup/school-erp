0676fddbfc3b605c41da2f778538f03793296061
Paul Kinuthia <pkinuthia10@gmail.com>
2018-07-14 12:44:08 +0300

    Finance/revised (#67)
    * integrate & configure eslint and prettier
    
    * refine fee item crud
    
    * finane item editing

---
+++ app_dir/static/js/finance/item/containers/EditItem.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+export class EditItems extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: '',
+      backspaceRemoves: true,
+      multi: true,
+      creatable: false
+    };
+  }
+  handleSelectChange = (selectedOption) => {
+    console.warn(selectedOption);
+    this.setState({ selectedOption });
+    // selectedOption can be null when the `x` (close) button is clicked
+    if (selectedOption) {
+      console.log(`Selected: ${selectedOption.label}`);
+    }
+  }
+  getUsers = (input) => {
+    if (!input) {
+      return Promise.resolve({ options: [] });
+    }
+
+    return fetch(`https://api.github.com/search/users?q=${input}`)
+      .then((response) => response.json())
+      .then((json) => {
+        return { options: json.items };
+      });
+  }
+  toggleBackspaceRemoves () {
+    this.setState({
+      backspaceRemoves: !this.state.backspaceRemoves
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Select.AsyncCreatable
+          multi={true}
+          value={this.state.selectedOption}
+          onChange={this.handleSelectChange}
+          //   onValueClick={this.gotoUser}
+          valueKey="id" labelKey="login"
+          loadOptions={this.getUsers}
+          backspaceRemoves={this.state.backspaceRemoves}
+        />
+      </div>
+    );
+  }
+}
+
+export default EditItems;
